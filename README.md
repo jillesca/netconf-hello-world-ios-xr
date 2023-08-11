c88e903039fc4f6def3102b1703000637fe578ca
jillesca <jillesca@cisco.com>
2023-08-11 16:52:21 +0100

    retrieving hostname using openconfig-system

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # XR Netconf Test Connection
 
-Simple hello world in netconf to test connectivity to ios-xr
+Simple hello world in netconf to test connectivity to IOS-XR
+
+Restrives hostname using [openconfig-system.yang](https://github.com/openconfig/public/blob/master/release/models/system/openconfig-system.yang)
 
 pip install -r requirements.txt
--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,5 @@
+import logging
+import xmltodict
 from ncclient import manager
 from ncclient.operations.errors import TimeoutExpiredError
 from ncclient.transport.errors import SSHError, AuthenticationError
@@ -7,12 +9,26 @@ PORT = 830
 TIMEOUT = 60
 USERNAME = "admin"
 PASSWORD = "C1sco12345"
+LOOK_FOR_KEYS = False
 HOSTKEY_VERIFY = False
 DEVICE = "sandbox-iosxr-1.cisco.com"
-LOOK_FOR_KEYS = False
+
+logging.basicConfig(
+    level=logging.INFO,
+)
+
+hostname_filter = """
+    <filter xmlns="urn:ietf:params:xml:ns:netconf:base:1.0"> 
+        <system xmlns="http://openconfig.net/yang/system">
+            <config>
+                <hostname />
+            </config>
+        </system>
+    </filter>
+"""
 
 
-def connect() -> None:
+def connect() -> str:
     with manager.connect(
         host=DEVICE,
         port=PORT,
@@ -22,21 +38,28 @@ def connect() -> None:
         look_for_keys=LOOK_FOR_KEYS,
         manager_params={"timeout": TIMEOUT},
     ) as m:
-        test_connection(m)
+        return get_hostname(m)
 
 
-def test_connection(m: manager) -> None:
-    host = m._session.host
-    connected = m._session._connected
-    authenticated = m._session.transport.authenticated
-    assert connected
-    assert authenticated
-    print(f"Session {host=}, {connected=}, {authenticated=}")
+def get_hostname(m: manager) -> str:
+    return m.get_config(source="running", filter=hostname_filter)
+
+
+def print_results(config: str) -> None:
+    parsed_cfg = parse_xml_to_dict(config)
+    hostname = parsed_cfg["rpc-reply"]["data"]["system"]["config"]["hostname"]
+    msg = f"!!! Successfully retrieve data from {hostname} !!!"
+    logging.info(msg)
+    print(msg)
+
+
+def parse_xml_to_dict(xml: str) -> dict:
+    return xmltodict.parse(xml.xml)
 
 
 def main():
     try:
-        connect()
+        print_results(connect())
     except AuthenticationError as err:
         print(f"Authentication Failed: {err=}")
 
